37764321e7b9a96cfc5c1248fa46fde96ab2e582
Qi-You Yu <qiyo.you@gmail.com>
2021-01-11 11:18:20 +0800

    Update Thrift2HBase.py

--- Thrift2HBase.py
+++ Thrift2HBase.py
@@ -17,4 +17,31 @@ class HBaseConnector():
     
     def __init__(self, topic_name, config_dict, input_table):
         if HBaseConnector.client == None:
+            hhost = str(config_dict['HBASE_ZOOKEEPER_QUORUM']).split(',')[1])
+            hport = 9090
+            socket = TSocket.TSocket(hhost, hport)
+            transport = TTransport.TBufferedTransport(socket)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            HBaseConnector.client = Client(protocol)
+            transport.open()
+        else:
+            pass
+        ## Get key work for each topic used in rowkey in hbase
+        self.from_topic = topic_name
+        self.topic = self.topicDict[topic_name]
+        self.input_table = input_table
+        
+    def get_offset_hbase(self, partition_number):
+        from_offsets = {}
+        for part in range(partition_number):
+            topic_partition = TopicAndPartition(self.from_topic, part)
+            rkey = '-uba:' + str(self.topic) + ':p' + str(part)
+            tget = TGet(row=rkey)
+            try:
+                offset = int(self.client.get(self.input_table, tget).columnValues[0].value)
+                from_offsets[topic_partition]
+            except IndexError:
+                from_offsets = None
+                break
+        return from_offsets
 
