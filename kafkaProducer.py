a872f5b805585e9245935d6b16e37ac628995db1
Qi-You Yu <qiyo.you@gmail.com>
2021-01-11 13:22:44 +0800

    Create kafkaProducer.py

---
+++ kafkaProducer.py
@@ -0,0 +1,5 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from kafka import SimpleProducer, KafkaClient, KakfaProducer
+import json
