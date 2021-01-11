608b03e3077c52eb4771888d9d28ec4ee3ee062d
Qi-You Yu <qiyo.you@gmail.com>
2021-01-11 14:26:56 +0800

    Update mysqlConnection.py

--- mysqlConnection.py
+++ mysqlConnection.py
@@ -1,3 +1,23 @@
 from pymysql import *
 
 class MysqlConnector():
+  
+    connector = None
+    cursor = None
+    
+    def __init__(self, host, port, database, user, password):
+        if (MysqlCOnnector.connector == None or MysqlCOnnector.cursor == None):
+            MysqlCOnnector.connector = connect(host=host, port=port, database=database, user=user, password=password)
+            MysqlCOnnector.cursor = MysqlCOnnector.connector.cursor()
+        else:
+            pass
+          
+    def execute(self, sql_string):
+        self.cursor.execute(sql_string)
+        self.connector.commit()
+        
+def insert_to_mysql(dosqls, host, port, database, user, password):
+    mysql = MysqlConnector(host=host, port=port, database=database, user=user, password=password)
+    mysql.execute(dosqls)
+            
+            
