189f50063a510b6a2b52abad37cdb506cbf03e65
Qi-You Yu <qiyo.you@gmail.com>
2021-01-11 11:41:56 +0800

    Update apolloConnection.py

--- apolloConnection.py
+++ apolloConnection.py
@@ -1,1 +1,9 @@
 from pyapollo import ApolloClient
+
+## build a function to get config from apollo (appid: public-dip-etl)
+client = ApolloClient(app_id = 'public-uba-etl',
+                      cluster = 'default',
+                      config_server_url = 'http://prod.apolloph.net:8080')
+
+def get_config():
+    return client.get_values()
